533f5377d8a9fe67acfc55e120c906bc8ac05cf2
robotian <robotian@gmail.com>
2023-09-07 02:11:09 +0000

    initial commit

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ set -e
 vcs import < src/ros2.repos src
 sudo apt-get update
 rosdep update
-rosdep install --from-paths src --ignore-src -y
+rosdep install -y --from-paths . --ignore-src --skip-keys="gz-transport12 gz-sim7 gz-math7 gz-msgs9"
