34452b7dc5cc4317394d0b7c3c090f5fcc9c59e2
robotian <robotian@gmail.com>
2023-09-14 09:45:22 -0400

    geometery2 added

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ BUILD_TYPE=RelWithDebInfo
 colcon build \
         --symlink-install \
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
-        -Wall -Wextra -Wpedantic
+        -Wall -Wextra -Wpedantic \
+        --allow-overriding geometry2 tf2 tf2_eigen tf2_geometry_msgs tf2_ros tf2_sensor_msgs
